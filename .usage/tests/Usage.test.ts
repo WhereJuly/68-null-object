e84a56d5ae01979cd9dcfaff48fcefe2fdc8acae
WhereJuly <where.july@googlemail.com>
2025-06-18 17:41:35 +0300

    Update (tests): usage tests

--- .usage/tests/Usage.test.ts
+++ .usage/tests/Usage.test.ts
@@ -2,10 +2,39 @@
 
 import { describe, expect, it } from 'vitest';
 
-describe('Null-Objet Usage Test', () => {
+import { noop, nullObject } from '@wherejuly/null-object';
 
-    it.todo('Should test...', () => {
-        expect(1).toEqual(1);
+describe('Null-Object & noop Function Usage Test', () => {
+
+    it('Should access properties or methods on Null-Object', () => {
+        const actual = nullObject();
+
+        expect(actual.dummy).toBeInstanceOf(Function);
+        expect(actual.dummy.another).toBeInstanceOf(Function);
+        expect(actual.dummy().another.more()).toBeInstanceOf(Function);
+        expect(actual.method()).toBeInstanceOf(Function);
+        expect(actual.method(123).another('abc')).toBeInstanceOf(Function);
+    });
+
+    it('Should be able to silently accept assignments to properties', () => {
+        const actual = nullObject<{ dummy: any; }>();
+
+        actual.dummy = 123;
+        actual.dummy.another = 'abc';
+
+        expect(actual.dummy).toBeInstanceOf(Function);
+        expect(actual.dummy.another).toBeInstanceOf(Function);
+    });
+
+    it('Should output a custom null-object identity name', () => {
+        const fixture = 'Fixture name';
+        const actual = nullObject(fixture);
+
+        expect(actual.toString()).toEqual(`[NullObject: ${fixture}]`); // NOSONAR
+    });
+
+    it('Should accept any arguments without throwing', () => {
+        expect(() => noop(1, 'a', true, null)).not.toThrow();
     });
 
 });
